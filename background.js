4328a99bd97fa989973648d3f68c7509732bbc17
HaveSechs <128012859+HaveSechs@users.noreply.github.com>
2023-03-25 09:00:31 -0500

    Update background.js
    updated list of urls

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
-var blockedUrls = ["adsafeprotected.com", "googleads.g.doubleclick.net", "cm.g.doubleclick.net", "ad.doubleclick.net", "pagead2.googlesyndication.com", "securepubads.g.doubleclick.net", "s.yimg.com/dy/ads"];
+var blockedUrls = [
+	"adsafeprotected.com", 
+	"googleads.g.doubleclick.net", 
+	"cm.g.doubleclick.net", 
+	"ad.doubleclick.net",
+	"pagead2.googlesyndication.com",
+	"securepubads.g.doubleclick.net", 
+	"s.yimg.com/dy/ads", 
+	"ras.yahoo.com/adcount", 
+	"s.yimg.com/cb/af/adfeedback", 
+	"yahoo.com/admax", 
+	"advertising.com/admax", 
+	"googletagservices.com/dcm/dcmads.js", 
+	"aka-cdn.adtechus.com/media/moat/adtechbrands092348fjlsmdhlwsl239fh3df/moatad.js", 
+	"ad.wsod.com"
+];
 
 chrome.webRequest.onBeforeRequest.addListener( function(details) {
     if (blockedUrls.some(function(url) { return details.url.includes(url); })) {
